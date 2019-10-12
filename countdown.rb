e883ae5675e09f3b1a5ea4ac50084a57bf0a19fa
Derric Fragosa <fderric@gmail.com>
2019-10-12 02:05:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,19 @@
-def countdown_with_sleep
-  #PAUSE LOOP takes in one intger argument for the countdown
-end
+def countdown_with_sleep(number)
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -=1 
+  sleep 1.second
+  end
+  return "HAPPY NEW YEAR!"
+ end
+ # sleep 1.seconds
+  #PAUSE LOOP takes in one intger argument for the countdown 
 
-
-#write your code here
 
 def countdown(number)
-  
   while number > 0 
   puts "#{number} SECOND(S)!"
   number -=1
   end 
-  
- return "HAPPY NEW YEAR!"
- 
+   return "HAPPY NEW YEAR!"
 end
